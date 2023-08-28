5fd8b917bbf8c2b7f5bef92ff95681aa776f073f
nadiaseleem <nadiaseleem97@gmail.com>
2023-08-28 13:15:27 +0300

    + change task status

--- app/src/main/java/com/example/todo/ui/home/TasksFragment.kt
+++ app/src/main/java/com/example/todo/ui/home/TasksFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.todo.R
 import com.example.todo.dao.TasksDao
 import com.example.todo.databinding.FragmentTasksBinding
 import com.example.todo.db.TasksDatabase
@@ -33,12 +35,20 @@ class TasksFragment : Fragment() {
 
     private fun initRecyclerView() {
         binding.rvTasks.adapter = tasksAdapter
+        tasksAdapter.onButtonClickedListener =
+            TasksAdapter.OnButtonClickedListener { position, task ->
+                task.idDone = !task.idDone
+                dao.updateTask(task)
+                tasksAdapter.updateTask(task, position)
+            }
     }
 
     override fun onStart() {
         super.onStart()
         dao = TasksDatabase.getInstance(requireContext()).tasksDao()
         loadAllTasks()
+        tasksAdapter.setColor(ContextCompat.getColor(requireContext(), R.color.blue))
+
     }
 
     fun loadAllTasks() {
--- app/src/main/java/com/example/todo/ui/home/adapter/TasksAdapter.kt
+++ app/src/main/java/com/example/todo/ui/home/adapter/TasksAdapter.kt
@@ -1,21 +1,48 @@
 package com.example.todo.ui.home.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.R
 import com.example.todo.databinding.ItemTaskBinding
 import com.example.todo.model.Task
 import com.example.todo.utils.getHourIn12
 import com.example.todo.utils.getTimeAmPm
 import java.util.Calendar
 
-class TasksAdapter(var tasks: MutableList<Task>? = null) :
+class TasksAdapter(var color: Int? = null, var tasks: MutableList<Task>? = null) :
     RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
-    inner class ViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: ItemTaskBinding, val color: Int?) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun changeTaskStatus(isDone: Boolean, color: Int? = null) {
+            if (isDone) {
+                binding.draggingBar.setBackgroundColor(Color.GREEN)
+                binding.title.setTextColor(Color.GREEN)
+                binding.btnTaskIsDone.setBackgroundResource(R.drawable.done)
+                binding.btnTaskIsDone.setImageResource(0)
+
+
+            } else {
+                color?.let {
+                    binding.title.setTextColor(it)
+                    binding.draggingBar.setBackgroundColor(it)
+                }
+                binding.btnTaskIsDone.setBackgroundResource(R.drawable.button_bg)
+                binding.btnTaskIsDone.setImageResource(R.drawable.ic_check)
+
+            }
+        }
+    }
+
+    fun setColor(color: Int) {
+        this.color = color
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        return ViewHolder(binding, color)
     }
 
     override fun getItemCount(): Int = tasks?.size ?: 0
@@ -24,11 +51,18 @@ class TasksAdapter(var tasks: MutableList<Task>? = null) :
         holder.binding.title.text = task.title
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = task.dateTime!!
+        holder.changeTaskStatus(task.idDone, color)
 
         val hr = calendar.get(Calendar.HOUR)
         val min = calendar.get(Calendar.MINUTE)
         val minutesString = if (min == 0) "00" else min.toString()
         holder.binding.time.text = "${getHourIn12(hr)}:$minutesString ${getTimeAmPm(hr)}"
+
+        onButtonClickedListener?.let { onButtonClickedListener ->
+            holder.binding.btnTaskIsDone.setOnClickListener {
+                onButtonClickedListener.onButtonClicked(position, task)
+            }
+        }
 
     }
 
@@ -39,6 +73,13 @@ class TasksAdapter(var tasks: MutableList<Task>? = null) :
 
     fun updateTask(task: Task, position: Int) {
         this.tasks?.set(position, task)
-        notifyItemInserted(position)
+        notifyItemChanged(position)
     }
+
+    var onButtonClickedListener: OnButtonClickedListener? = null
+
+    fun interface OnButtonClickedListener {
+        fun onButtonClicked(position: Int, task: Task)
+    }
+
 }
